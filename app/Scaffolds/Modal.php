fcc76aac485042d7acbf994e27f3170e701113e9
Wojciech Przybyła <contact@wpww.pl>
2026-04-05 14:35:27 +0200

    ⚓ modals transferred

---
+++ app/Scaffolds/Modal.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Scaffolds;
+
+use App\Scaffolds\Shipyard\Modal as ShipyardModal;
+
+class Modal extends ShipyardModal
+{
+    protected static function items(): array
+    {
+        return [
+
+        ];
+    }
+}
